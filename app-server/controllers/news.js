894b0a1109f8b4ab1a221b77cba938345d7b96be
Richard Goshen <rick.goshen@gmail.com>
2026-03-16 21:41:22 -0700

    extract news data to JSON and templatize news view

--- app-server/controllers/news.js
+++ app-server/controllers/news.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+
+const { latestNews, vacationTips, featured } = JSON.parse(
+    fs.readFileSync('./data/news.json', 'utf-8')
+);
+
 /* GET news view */
 const news = function (req, res) {
-    res.render('news', { title: 'Travlr Getaways - News' });
+    res.render('news', { title: 'Travlr Getaways - News', latestNews, vacationTips, featured });
 };
 
 module.exports = { news };
