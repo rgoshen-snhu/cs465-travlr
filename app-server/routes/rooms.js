badd0657c87dc2b06c4368094b1821f69ddbc219
Richard Goshen <rick.goshen@gmail.com>
2026-03-16 20:14:28 -0700

    add rooms controller, route, and view template

---
+++ app-server/routes/rooms.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/rooms');
+
+/* GET rooms page. */
+router.get('/', controller.rooms);
+
+module.exports = router;
