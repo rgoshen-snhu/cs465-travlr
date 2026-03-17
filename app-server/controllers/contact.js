2fc5fb46924314d64da58a0bf3617b7f6d1991a4
Richard Goshen <rick.goshen@gmail.com>
2026-03-16 21:43:23 -0700

    extract contact data to JSON and templatize contact view

--- app-server/controllers/contact.js
+++ app-server/controllers/contact.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+
+const contact = JSON.parse(fs.readFileSync('./data/contact.json', 'utf-8'));
+
 /* GET contact view */
-const contact = function (req, res) {
-    res.render('contact', { title: 'Travlr Getaways - Contact' });
+const contactHandler = function (req, res) {
+    res.render('contact', { title: 'Travlr Getaways - Contact', contact });
 };
 
-module.exports = { contact };
+module.exports = { contact: contactHandler };
