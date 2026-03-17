f4f8ad7478560fcb02feb929236592c71bad27fd
Richard Goshen <rick.goshen@gmail.com>
2026-03-16 21:42:27 -0700

    extract about data to JSON and templatize about view

--- app-server/controllers/about.js
+++ app-server/controllers/about.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+
+const { intro, sections } = JSON.parse(fs.readFileSync('./data/about.json', 'utf-8'));
+
 /* GET about view */
 const about = function (req, res) {
-    res.render('about', { title: 'Travlr Getaways - About' });
+    res.render('about', { title: 'Travlr Getaways - About', intro, sections });
 };
 
 module.exports = { about };
