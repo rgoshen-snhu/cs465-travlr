cf9bef7a278b34621fd6bc5a89322978a71f2967
Richard Goshen <rick.goshen@gmail.com>
2026-04-10 18:56:08 -0700

    feat: implement AddTripComponent with form validation and submission; update routing and trip data service

--- app-admin/src/app/services/trip-data.service.ts
+++ app-admin/src/app/services/trip-data.service.ts
@@ -9,9 +9,13 @@ import { Trip } from '../models/trips';
 export class TripDataService {
 
   constructor(private http: HttpClient) { }
+  url = 'http://localhost:3000/api/trips';
 
   getTrips(): Observable<Trip[]> {
-    let url = 'http://localhost:3000/api/trips';
-    return this.http.get<Trip[]>(url);
+    return this.http.get<Trip[]>(this.url);
+  }
+
+  addTrip(formData: Trip): Observable<Trip> {
+    return this.http.post<Trip>(this.url, formData);
   }
 }
