ff7e70e8cc207857c4d5e63b5fbe227639168043
Richard Goshen <rick.goshen@gmail.com>
2026-04-12 16:26:32 -0700

    feat: add JWT authentication middleware to protect trip creation and update routes

--- app-api/routes/index.js
+++ app-api/routes/index.js
@@ -2,6 +2,35 @@ const express = require('express');
 const router = express.Router();
 const tripsController = require('../controllers/trips');
 const authController = require('../controllers/authentication');
+const jwt = require('jsonwebtoken'); // Enable JSON Web Tokens
+
+// Method to authenticate the JWT token
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (authHeader === null) {
+        console.error('Auth Header Required but NOT PRESENT!');
+        return res.sendStatus(401);
+    }
+
+    let headers = authHeader.split(' ');
+    if (headers.length < 1) {
+        console.error('Not enough tokens in Auth Header: ' + headers.length);
+        return res.sendStatus(501);
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (token === null) {
+        console.error('NULL Bearer Token');
+        return res.sendStatus(401);
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET, (err, verified) => {
+        if (err) return res.sendStatus(401).json('Token Validation Error!');
+        req.auth = verified;
+        next();
+    });
+}
 
 
 // Define the route for user registration and login
@@ -14,12 +43,12 @@ router.route('/login')
 router
     .route('/trips')
     .get(tripsController.tripsList) // GET: /trips - get a list of all trips
-    .post(tripsController.tripsAddTrip); // POST: /trips - create a new trip (not implemented yet)
+    .post(authenticateJWT, tripsController.tripsAddTrip); // POST: /trips - create a new trip (not implemented yet)
 
 // Define the route for getting a specific trip by code
 router
     .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode) // GET: /trips/:tripCode - get a specific trip by code
-    .put(tripsController.tripsUpdateTrip); // PUT: /trips/:tripCode - update a specific trip by code (not implemented yet)
+    .put(authenticateJWT, tripsController.tripsUpdateTrip); // PUT: /trips/:tripCode - update a specific trip by code (not implemented yet)
 
 module.exports = router;
